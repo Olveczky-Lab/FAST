ab83e4348a2e2a90c96d84c22e334f5c249704d3
olveczkylab <olveczky@fas.harvard.edu>
2017-06-27 19:38:53 -0400

    Update StopClustering.ps1

--- Releases/1.0/StopClustering.ps1
+++ Releases/1.0/StopClustering.ps1
@@ -2,6 +2,6 @@
 $workernodes=@("192.168.0.201:8000","192.168.0.202:8000","192.168.0.203:8000","192.168.0.204:8000","192.168.0.205:8000")
 for ($j=0;$j -lt $workernodes.length;$j++) {
 	$workernode = $workernodes[$j]
-	iex "& '$prc' http://$workernode Administrator olabmstr!1 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' 'Stop-Process -ProcessName TitanSpikeClusterer' C:\Titanic\Clusterer"
+	iex "& '$prc' http://$workernode Administrator password 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' 'Stop-Process -ProcessName TitanSpikeClusterer' C:\Titanic\Clusterer"
 }
 Start-Sleep -Seconds 2
