dd4dababba66ea926f6c26a9d3cb840da8850650
olveczkylab <olveczky@fas.harvard.edu>
2017-06-28 14:28:23 -0400

    Update StartSnippeting.ps1

--- Releases/1.0/MasterNode/StartSnippeting.ps1
+++ Releases/1.0/MasterNode/StartSnippeting.ps1
@@ -22,7 +22,7 @@ else {
 			for ($j=0;$j -lt $workernodes.length;$j++) {
 				$workernode = $workernodes[$j]
 				for ($i=0;$i -lt 4;$i++) {
-					iex "& '$prc' http://$workernode Administrator password $cmd 'SnippetWorker $hostname $settingsfile $workerendpoint $datafile $outpath $j-$i' C:\Titanic\Snippeter"
+					iex "& '$prc' http://$workernode username password $cmd 'SnippetWorker $hostname $settingsfile $workerendpoint $datafile $outpath $j-$i' C:\Titanic\Snippeter"
 				}
 			}
 
