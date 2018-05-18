59422af5b12bb3f641a3d25ee8e6a3d2f07e4c16
olveczkylab <olveczky@fas.harvard.edu>
2018-05-18 11:37:51 -0400

    Update SetupSnippeter.ps1

--- FAST source/TitanSpikeSnippeter/SnippetMaster/SetupSnippeter.ps1
+++ FAST source/TitanSpikeSnippeter/SnippetMaster/SetupSnippeter.ps1
@@ -1,11 +1,11 @@
-﻿. \\140.247.178.88\Rajesh\RatControlOp\PowerShell\ImportExport-PSCredential.ps1
+﻿. \\ipaddress\Rajesh\RatControlOp\PowerShell\ImportExport-PSCredential.ps1
 $Cred = Import-PSCredential C:\Titanic\PSCred.enc.xml
 for ($i=1;$i -le 5;$i++) {
 	Invoke-Command -ComputerName "192.168.0.20$i" -Authentication Credssp -Credential $Cred -ScriptBlock {
-		net use \\140.247.178.88\Rajesh olabmstrpwdvrysecure /user:rpoddar
+		net use \\ipaddress\Rajesh password /user:rpoddar
 		Remove-Item 'C:\Titanic\Snippeter' -Force -Recurse
-		Copy-Item '\\140.247.178.88\Rajesh\Ephys\Data Analysis\FSharp\TitanSpikeSnippeter\SnippetMaster\bin\Release' 'C:\Titanic\Snippeter' -Force -Recurse -Container
-		net use /delete \\140.247.178.88\Rajesh /yes
+		Copy-Item '\\ipaddress\Rajesh\Ephys\Data Analysis\FSharp\TitanSpikeSnippeter\SnippetMaster\bin\Release' 'C:\Titanic\Snippeter' -Force -Recurse -Container
+		net use /delete \\ipaddress\Rajesh /yes
 	}
 }
 
