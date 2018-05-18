a3d3eb11e633edcdfe496f0ed6affc7194084454
olveczkylab <olveczky@fas.harvard.edu>
2018-05-18 11:38:33 -0400

    Update NetUse.ps1

--- FAST source/TitanSpikeSnippeter/SnippetMaster/NetUse.ps1
+++ FAST source/TitanSpikeSnippeter/SnippetMaster/NetUse.ps1
@@ -1,4 +1,4 @@
-﻿$prc="\\140.247.178.88\Rajesh\Ephys\ProcessRunnerService\ProcessRunnerClient\ProcessRunnerClient\bin\Release\ProcessRunnerClient.exe"
+﻿$prc="\\ipaddress\Rajesh\Ephys\ProcessRunnerService\ProcessRunnerClient\ProcessRunnerClient\bin\Release\ProcessRunnerClient.exe"
 for ($j=1;$j -le 5;$j++) {
-	iex "& '$prc' http://192.168.0.20$j`:8000 Administrator olabmstr!1 net.exe 'use \\192.168.0.10\asheshdhawale elawahdhsehsa! /user:asheshdhawale' C:\Titanic\Snippeter"
+	iex "& '$prc' http://192.168.0.20$j`:8000 Administrator olabmstr!1 net.exe 'use \\ipaddress\asheshdhawale password /user:asheshdhawale' C:\Titanic\Snippeter"
 }
